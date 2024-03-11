bad4b1640e55dd56590a196eb96e3580b29d1b61
antah96 <125658885+antah96@users.noreply.github.com>
2024-03-11 23:41:42 +0300

    Update TempFileExample.java

--- TempFileExample.java
+++ TempFileExample.java
@@ -8,14 +8,20 @@ public class TempFileExample {
     public static void main(String[] args) {
 
         try {
-            File temp = File.createTempFile("temp", ".txt");
-            temp.deleteOnExit();
+            new File("/myDirectory/myfile.txt");  // Compliant
 
-            BufferedWriter out = new BufferedWriter(new FileWriter(temp));
-            out.write("aString");
-            out.close();
+            File.createTempFile("prefix", "suffix", new File("/mySecureDirectory"));  // Compliant
 
-            System.out.println("Temporary file created with insecure permissions. Now an attacker can perform malicious actions using the file.");
+            if(SystemUtils.IS_OS_UNIX) {
+              FileAttribute<Set<PosixFilePermission>> attr = PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rwx------"));
+              Files.createTempFile("prefix", "suffix", attr); // Compliant
+            }
+            else {
+              File f = Files.createTempFile("prefix", "suffix").toFile();  // Compliant
+              f.setReadable(true, true);
+              f.setWritable(true, true);
+              f.setExecutable(true, true);
+            }
         } catch (IOException e) {
             System.err.println("Failed to create temporary file: " + e.getMessage());
         }
